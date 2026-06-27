8c71912aa62ae912bdeed457d7bbfbda38ba1cc3
Sanch3zzzz <gustavosp1209@protonmail.com>
2026-06-27 00:00:10 -0300

    update dotfiles 27/06/2026 00:00

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -121,7 +121,7 @@ os.execute("systemctl --user enable --now hyprpolkitagent.service &")
 hl.config({
     general = {
         gaps_in  = 2,
-        gaps_out = 10,
+        gaps_out = 2,
 
         border_size = 1,
 
